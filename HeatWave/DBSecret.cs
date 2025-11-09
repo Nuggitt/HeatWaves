76a442a299e68e45015c1d28196e864df431059f
Nuggitt <phulip.nielsen@gmail.com>
2025-11-09 01:19:51 +0100

    nothing...

--- HeatWave/DBSecret.cs
+++ HeatWave/DBSecret.cs
@@ -4,7 +4,7 @@
     {
 
         public static readonly string ConnectionStringSimply =
-            "Data Source=myzealandstudioserver.database.windows.net;Initial Catalog=mystudioDB;User ID=phil2629;Password=Holger1208!;Connect Timeout=60;Trust Server Certificate=True";
+            "Data Source=myzealandstudioserver.database.windows.net;Initial Catalog=mystudioDB;User ID=***;Password=*****!;Connect Timeout=60;Trust Server Certificate=True";
 
     }
 }
